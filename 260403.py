2059f74d759574945be7397bf6452534b917bac8
hyeok9865 <hyeok9865@gmail.com>
2026-04-03 16:15:28 +0900

    Add files via upload

---
+++ 260403.py
@@ -0,0 +1,48 @@
+#for i in range(1,101):
+#    print(i)
+    
+#for i in range(2,101,2):
+#    print(i)
+
+#for i in range(1,101):
+#    if i%2==0:
+#        print(i)
+
+#sum=0
+#for i in range(1,101):
+#    sum=sum+i
+#print(sum)
+
+#evensum=0
+#oddsum=0
+#for i in range(1,101):
+#    if i%2==0:
+#        evensum=evensum+i
+#    else:
+#        oddsum=oddsum+i
+#print("짝수합: ", evensum)
+#print("홀수합: ", oddsum)
+
+#i=1
+#while i<=100:
+#    print(i)
+#    i+=1
+
+#i=1
+#while True:
+#    print(i)
+#    i+=1
+
+#sum=0
+#i=1
+#while sum<=3000:
+#    print(sum)
+#    sum=sum+i
+#    i+=1
+
+#for i in [1,2,3,4,5]:
+#    print(i)
+
+
+#for i in "Hello":
+#    print(i)
